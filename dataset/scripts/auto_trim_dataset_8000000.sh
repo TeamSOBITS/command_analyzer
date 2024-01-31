27d19b6bb2db1c773926804e59a021f8e2335adf
h.mie <hirotakamie@gmail.com>
2024-01-31 14:43:50 +0900

    update example

--- dataset/scripts/auto_trim_dataset_8000000.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TEXT_NUM=$((wc -l <../data/increased_dataset.txt))
-# TEXT_NUM=$(wc -l <../data/$1)
-TEXT_NUM=$(wc -l <../data/increased_dataset.txt)
-# text_num=$(bc $TEXT_NUM / 1000)
-echo 8000000
-# python3 trim_dataset.py $((($TEXT_NUM/1000)*1000)) $1
-python3 trim_dataset.py 8000000 increased_dataset.txt
